9da01db6fd43aa41c0ca467771f9def9ef411513
babijana <babijanaj.23@cse.mrt.ac.lk>
2026-08-01 08:46:37 +0530

    Build login page UI

---
+++ src/components/forms/LoginForm.tsx
@@ -0,0 +1,86 @@
+import Card from "../ui/Card";
+import Input from "../ui/Input";
+import Button from "../ui/Button";
+
+export default function LoginForm() {
+    return (
+        <Card className="w-full max-w-lg p-10">
+
+            <h1 className="text-3xl font-bold text-center">
+                Welcome Back
+            </h1>
+
+            <p className="mt-2 text-center text-gray-600">
+                Sign in to continue to ResumeAI
+            </p>
+
+            {/* Form */}
+            <div className="mt-8 space-y-5">
+
+                {/* Email */}
+                <div>
+                    <label className="mb-2 block font-medium">
+                        Email
+                    </label>
+
+                    <Input
+                        type="email"
+                        placeholder="Enter your email"
+                    />
+                </div>
+
+                {/* Password */}
+                <div>
+                    <label className="mb-2 block font-medium">
+                        Password
+                    </label>
+
+                    <Input
+                        type="password"
+                        placeholder="Enter your password"
+                    />
+                </div>
+                <div className="flex items-center justify-between text-sm">
+
+                    <label className="flex items-center gap-2">
+
+                        <input
+                            type="checkbox"
+                            className="h-4 w-4 rounded border-gray-300 accent-indigo-600"
+                        />
+
+                        Remember me
+
+                    </label>
+
+                    <a
+                        href="#"
+                        className="font-medium text-indigo-600 hover:underline"
+                    >
+                        Forgot Password?
+                    </a>
+
+                </div>
+
+                {/* Login Button */}
+                <Button className="w-full">
+                    Login
+                </Button>
+                <p className="text-center text-sm text-gray-600">
+
+    Don't have an account?
+
+    <a
+        href="#"
+        className="ml-1 font-semibold text-indigo-600 hover:underline"
+    >
+        Register
+    </a>
+
+</p>
+
+            </div>
+
+        </Card>
+    );
+}
---
+++ src/pages/Login/LoginPage.tsx
@@ -0,0 +1,15 @@
+import LoginForm from "../../components/forms/LoginForm";
+
+export default function LoginPage() {
+    return (
+        <section className="min-h-screen bg-gray-50">
+
+            <div className="mx-auto flex min-h-screen max-w-7xl items-center justify-center px-6">
+
+                <LoginForm />
+
+            </div>
+
+        </section>
+    );
+}
